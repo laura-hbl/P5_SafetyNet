3208e9c4141af65a31d4ab3f4f5aa52ffe59a6dd
Laura <laura_habdul@hotmail.fr>
2020-08-08 10:43:33 +0200

    SafetyNetApplication Class implemented

--- src/main/java/com/safetynet/Alerts/SafetyNetApplication.java
+++ src/main/java/com/safetynet/Alerts/SafetyNetApplication.java
@@ -1,13 +1,25 @@
 package com.safetynet.Alerts;
 
+import com.safetynet.Alerts.data.StoredData;
+import com.safetynet.Alerts.util.DataReader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SafetyNetApplication {
 
+	@Value("${dataFile}")
+	private String dataFilePath;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetApplication.class, args);
+	}
+
+	@Bean
+	public StoredData readData() throws Exception {
+		return DataReader.readFile(dataFilePath);
 	}
 
 }
