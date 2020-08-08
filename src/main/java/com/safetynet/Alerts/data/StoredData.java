699294f3f0256382e0cc7894f63f79f6d8ff8ab1
Laura <laura_habdul@hotmail.fr>
2020-08-08 11:03:08 +0200

    Add StoredData class

---
+++ src/main/java/com/safetynet/Alerts/data/StoredData.java
@@ -0,0 +1,33 @@
+package com.safetynet.Alerts.data;
+
+import com.safetynet.Alerts.model.FireStation;
+import com.safetynet.Alerts.model.MedicalRecord;
+import com.safetynet.Alerts.model.Person;
+
+import java.util.List;
+
+public class StoredData {
+
+    private List<Person> personList;
+    private List<FireStation> fireStationList;
+    private List<MedicalRecord> medicalRecordList;
+
+    public StoredData(List<Person> personList, List<FireStation> fireStationList, List<MedicalRecord> medicalRecordList) {
+        this.personList = personList;
+        this.fireStationList = fireStationList;
+        this.medicalRecordList = medicalRecordList;
+    }
+
+    public List<Person> getPersonList() {
+        return personList;
+    }
+
+    public List<FireStation> getFireStationList() {
+        return fireStationList;
+    }
+
+    public List<MedicalRecord> getMedicalRecordList() {
+        return medicalRecordList;
+    }
+
+}
