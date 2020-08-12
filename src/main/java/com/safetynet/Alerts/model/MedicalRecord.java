78af2ba5e267701d894bc181de86da50f12fde6f
Laura <laura_habdul@hotmail.fr>
2020-08-12 22:09:26 +0200

    Add model class

---
+++ src/main/java/com/safetynet/Alerts/model/MedicalRecord.java
@@ -0,0 +1,61 @@
+package com.safetynet.Alerts.model;
+
+import java.util.List;
+
+public class MedicalRecord {
+
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+    private List<String> medications;
+    private List<String> allergies;
+
+
+    public MedicalRecord(String firstName, String lastName, String birthDate, List<String> medications, List<String> allergies) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.medications = medications;
+        this.allergies = allergies;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<String> getMedications() {
+        return medications;
+    }
+
+    public void setMedications(List<String> medications) {
+        this.medications = medications;
+    }
+
+    public List<String> getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(List<String> allergies) {
+        this.allergies = allergies;
+    }
+}
