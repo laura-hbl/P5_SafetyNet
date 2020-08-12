dcff4480340eeed6ea049987cee8312159586650
Laura <laura_habdul@hotmail.fr>
2020-08-12 22:30:21 +0200

    Create DTO classes: PersonDTO, MedicalRecordDTO and FireStationDTO

---
+++ src/main/java/com/safetynet/Alerts/dto/FireStationDTO.java
@@ -0,0 +1,29 @@
+package com.safetynet.Alerts.dto;
+
+public class FireStationDTO {
+
+    private String address;
+    private int station;
+
+    public FireStationDTO(String address, int station) {
+        this.address = address;
+        this.station = station;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getStation() {
+        return station;
+    }
+
+    public void setStation(int station) {
+        this.station = station;
+    }
+
+}
