3f4b85b483d8b7434afb614a40c67d074ca53cd6
Laura <laura_habdul@hotmail.fr>
2020-08-12 22:24:33 +0200

    Update FireStationRepository class and add methods

--- src/main/java/com/safetynet/Alerts/repository/FireStationRepository.java
+++ src/main/java/com/safetynet/Alerts/repository/FireStationRepository.java
@@ -5,24 +5,53 @@ import com.safetynet.Alerts.model.FireStation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class FireStationRepository {
 
+    Map<String, FireStation> fireStationMap = new HashMap<>();
+
     @Autowired
-    StoredData storedData;
-
-    public List<FireStation> getFireStationList() {
-        return storedData.getFireStationList();
+    public FireStationRepository(StoredData storedData) {
+        storedData.getFireStationList().forEach(fireS -> fireStationMap.put(fireS.getStation()
+                + fireS.getAddress(), fireS));
     }
 
-    public void save(FireStation fireStation) {
-        storedData.getFireStationList().add(fireStation);
+    public FireStation save(FireStation fireS) {
+        return fireStationMap.put(fireS.getStation() + fireS.getAddress(), fireS);
     }
 
-    public void delete(FireStation fireStation) {
-        storedData.getFireStationList().remove(fireStation);
+    public void delete(FireStation fireS) {
+        fireStationMap.remove(fireS.getStation() + fireS.getAddress());
     }
 
+    public FireStation find(FireStation fireS) {
+        return fireStationMap.get(fireS.getStation() + fireS.getAddress());
+    }
+
+    public FireStation findByAddress(String address) {
+        Collection<FireStation> fireStations = fireStationMap.values();
+
+        for (FireStation fireStation : fireStations) {
+            if (fireStation.getAddress().equals(address)) {
+                return fireStation;
+            }
+        }
+
+        return null;
+    }
+
+    public List<FireStation> findByStation(int station) {
+        Collection<FireStation> fireStations = fireStationMap.values();
+        List<FireStation> fireStationsByStation = new ArrayList<>();
+
+        for (FireStation fireS : fireStations) {
+            if (fireS.getStation() == station) {
+                fireStationsByStation.add(fireS);
+            }
+        }
+
+        return fireStationsByStation;
+    }
 }
