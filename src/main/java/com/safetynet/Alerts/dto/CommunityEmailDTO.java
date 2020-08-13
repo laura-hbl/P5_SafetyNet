9f6a16718d84d1f0e52e46f2994aca8a3dbb15a8
Laura <laura_habdul@hotmail.fr>
2020-08-13 09:47:39 +0200

    Add DTO class: PhoneAlertDTO, CommunityEmailDTO

---
+++ src/main/java/com/safetynet/Alerts/dto/CommunityEmailDTO.java
@@ -0,0 +1,20 @@
+package com.safetynet.Alerts.dto;
+
+import java.util.List;
+
+public class CommunityEmailDTO {
+
+    private List<String> emails;
+
+    public CommunityEmailDTO(List<String> emails) {
+        this.emails = emails;
+    }
+
+    public List<String> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<String> emails) {
+        this.emails = emails;
+    }
+}
---
+++ src/main/java/com/safetynet/Alerts/dto/PhoneAlertDTO.java
@@ -0,0 +1,20 @@
+package com.safetynet.Alerts.dto;
+
+import java.util.List;
+
+public class PhoneAlertDTO {
+
+    private List<String> phones;
+
+    public PhoneAlertDTO(List<String> phones) {
+        this.phones = phones;
+    }
+
+    public List<String> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<String> phones) {
+        this.phones = phones;
+    }
+}
