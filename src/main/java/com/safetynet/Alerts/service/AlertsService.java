db845d8ecd2615e79eeff8265df86dab24ef21da
Laura <laura_habdul@hotmail.fr>
2020-08-13 10:01:14 +0200

    Update AlertsService with functions: getPersonsByStation and getChildByAddress

--- src/main/java/com/safetynet/Alerts/service/AlertsService.java
+++ src/main/java/com/safetynet/Alerts/service/AlertsService.java
@@ -1,11 +1,13 @@
 package com.safetynet.Alerts.service;
 
-import com.safetynet.Alerts.dto.CommunityEmailDTO;
-import com.safetynet.Alerts.dto.PhoneAlertDTO;
-import com.safetynet.Alerts.model.Person;
+import com.safetynet.Alerts.dto.*;
+import com.safetynet.Alerts.model.*;
+import com.safetynet.Alerts.util.AgeCalculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,30 +23,57 @@ public class AlertsService {
     @Autowired
     private MedicalRecordService medicalRecordService;
 
-    public PhoneAlertDTO getPhonesByStation(int station) {
+    private static AgeCalculator ageCalculator = new AgeCalculator();
+    private static final int adultAge = 19;
+
+    public PersonsByStationDTO getPersonsByStation(int station) {
+        int adultCount = 0;
+        int childCount = 0;
+
         List<Person> persons = personService.getPersonList();
         List<String> addresses = fireStationService.getAddressesByStation(station);
-        List<String> phones = new ArrayList<>();
+        List<PersonStation> list = new ArrayList<>();
 
         for (Person pers : persons) {
             for (String address : addresses) {
                 if (pers.getAddress().equals(address)) {
-                    phones.add(pers.getPhone());
+                    MedicalRecord med = medicalRecordService.getMedicalRecordById(pers.getFirstName(), pers.getLastName());
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/d/yyyy");
+                    LocalDate birthDate = LocalDate.parse(med.getBirthDate(), formatter);
+                    int age = ageCalculator.getAge(birthDate);
+
+                    if (age < adultAge) {
+                        childCount++;
+                    } else {
+                        adultCount++;
+                    }
+                    list.add(new PersonStation(pers.getFirstName(), pers.getLastName(), pers.getAddress(),
+                            pers.getPhone()));
                 }
             }
         }
 
-        return new PhoneAlertDTO(phones);
+        return new PersonsByStationDTO(list, adultCount, childCount);
     }
 
-    public CommunityEmailDTO getEmailsByCity(String city) {
-        List<Person> personsByCity = personService.getPersonsByCity(city);
-        List<String> emails = new ArrayList<>();
+    public ChildAlertDTO getChildByAddress(String address) {
+        List<Person> personsByAddress = personService.getPersonsByAddress(address);
+        List<Child> childList = new ArrayList<>();
+        List<String> adultList = new ArrayList<>();
 
-        for (Person person : personsByCity) {
-            emails.add(person.getEmail());
+        for (Person pers : personsByAddress) {
+            MedicalRecord med = medicalRecordService.getMedicalRecordById(pers.getFirstName(), pers.getLastName());
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/d/yyyy");
+            LocalDate birthDate = LocalDate.parse(med.getBirthDate(), formatter);
+            int age = ageCalculator.getAge(birthDate);
+
+            if (age < adultAge) {
+                childList.add(new Child(pers.getFirstName(), pers.getLastName(), age));
+            } else {
+                adultList.add("FirstName : " + pers.getFirstName() + " LastName : " + pers.getLastName());
+            }
         }
 
-        return new CommunityEmailDTO(emails);
+        return new ChildAlertDTO(childList, adultList);
     }
 }
