2d16001d003711c026d3f10087aa0d07a251da6c
Laura <laura_habdul@hotmail.fr>
2020-08-13 09:57:15 +0200

    Create PersonStation and PersonsByStationDTO classes

---
+++ src/main/java/com/safetynet/Alerts/dto/PersonsByStationDTO.java
@@ -0,0 +1,42 @@
+package com.safetynet.Alerts.dto;
+
+import com.safetynet.Alerts.model.PersonStation;
+
+import java.util.List;
+
+public class PersonsByStationDTO {
+
+    private List<PersonStation> personsByStation;
+    private int adultNumber;
+    private int childNumber;
+
+    public PersonsByStationDTO(List<PersonStation> personsByStation, int adultNumber, int childNumber) {
+        this.personsByStation = personsByStation;
+        this.adultNumber = adultNumber;
+        this.childNumber = childNumber;
+    }
+
+    public List<PersonStation> getPersonsByStation() {
+        return personsByStation;
+    }
+
+    public void setPersonsByStation(List<PersonStation> personsByStation) {
+        this.personsByStation = personsByStation;
+    }
+
+    public int getAdultNumber() {
+        return adultNumber;
+    }
+
+    public void setAdultNumber(int adultNumber) {
+        this.adultNumber = adultNumber;
+    }
+
+    public int getChildNumber() {
+        return childNumber;
+    }
+
+    public void setChildNumber(int childNumber) {
+        this.childNumber = childNumber;
+    }
+}
---
+++ src/main/java/com/safetynet/Alerts/model/PersonStation.java
@@ -0,0 +1,48 @@
+package com.safetynet.Alerts.model;
+
+public class PersonStation {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phone;
+
+    public PersonStation(String firstName, String lastName, String address, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
