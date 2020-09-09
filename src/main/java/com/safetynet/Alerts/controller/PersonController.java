8b99fbe09e55e844603467fb37c95ef6183955d7
Laura <laura_habdul@hotmail.fr>
2020-09-10 01:12:44 +0200

    Refactor PersonController and create GetPerson method

--- src/main/java/com/safetynet/Alerts/controller/PersonController.java
+++ src/main/java/com/safetynet/Alerts/controller/PersonController.java
@@ -2,7 +2,7 @@ package com.safetynet.Alerts.controller;
 
 import com.safetynet.Alerts.dto.PersonDTO;
 import com.safetynet.Alerts.exception.BadRequestException;
-import com.safetynet.Alerts.model.Person;
+import com.safetynet.Alerts.service.IPersonService;
 import com.safetynet.Alerts.service.PersonService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,7 +16,7 @@ public class PersonController {
 
     private static final Logger LOGGER = LogManager.getLogger(PersonController.class);
 
-    PersonService personService;
+    IPersonService personService;
 
     @Autowired
     public PersonController(PersonService personService) {
@@ -24,41 +24,60 @@ public class PersonController {
     }
 
     @PostMapping("/person")
-    public ResponseEntity<Person> createPerson(@RequestBody PersonDTO person) {
+    public ResponseEntity<PersonDTO> createPerson(@RequestBody PersonDTO person) {
         LOGGER.debug("Person CREATE Request on : " + person.getFirstName() + " " + person.getLastName());
 
-        if (person == null) {
-            throw new BadRequestException("Bad request : missing request body");
+        if (person.getFirstName() == null || person.getFirstName().isEmpty() || person.getLastName() == null
+                || person.getLastName().isEmpty()) {
+            throw new BadRequestException("Bad request : missing or incomplete body request");
         }
-        Person personCreated = personService.createPerson(person);
+        PersonDTO personCreated = personService.createPerson(person);
 
         LOGGER.info("Person POST request - SUCCESS");
         return new ResponseEntity<>(personCreated, HttpStatus.CREATED);
     }
 
     @PutMapping("/person")
-    public ResponseEntity<Person> updatePerson(@RequestBody PersonDTO person) {
+    public ResponseEntity<PersonDTO> updatePerson(@RequestBody PersonDTO person) {
         LOGGER.debug("Person UPDATE Request on : " + person.getFirstName() + " " + person.getLastName());
 
-        if (person == null) {
-            throw new BadRequestException("Bad request : missing request body");
+        if (person.getFirstName() == null || person.getFirstName().isEmpty() || person.getLastName() == null
+                || person.getLastName().isEmpty()) {
+            throw new BadRequestException("Bad request : missing or incomplete body request");
         }
-        Person personUpdated = personService.updatePerson(person);
+        PersonDTO personUpdated = personService.updatePerson(person);
 
         LOGGER.info("Person PUT request - SUCCESS");
         return new ResponseEntity<>(personUpdated, HttpStatus.OK);
     }
 
     @DeleteMapping("/person")
-    public ResponseEntity<Void> deletePerson(@RequestBody PersonDTO person) {
-        LOGGER.debug("Person DELETE Request on : " + person.getFirstName() + " " + person.getLastName());
+    public ResponseEntity<Void> deletePerson(@RequestParam("firstName") String firstName,
+                                             @RequestParam("lastName") String lastName) {
+        LOGGER.debug("Person DELETE Request on : " + firstName + " " + lastName);
 
-        if (person == null) {
-            throw new BadRequestException("Bad request : missing request body");
+        if (firstName == null || firstName.trim().length() == 0 || lastName == null
+                || lastName.trim().length() == 0) {
+            throw new BadRequestException("Bad request : missing or incomplete parameter");
         }
-        personService.deletePerson(person);
+        personService.deletePerson(firstName, lastName);
 
         LOGGER.info("Person DELETE request - SUCCESS");
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/person")
+    public ResponseEntity<PersonDTO> getPerson(@RequestParam("firstName") String firstName,
+                                         @RequestParam("lastName") String lastName) {
+        LOGGER.debug("Person GET Request on : {} {}", firstName, lastName);
+
+        if (firstName == null || firstName.trim().length() == 0 || lastName == null
+                || lastName.trim().length() == 0) {
+            throw new BadRequestException("Bad request : missing or incomplete parameter");
+        }
+        PersonDTO personDTO = personService.getPersonById(firstName, lastName);
+
+        LOGGER.info("Person GET Request - SUCCESS");
+        return new ResponseEntity<>(personDTO, HttpStatus.OK);
     }
 }
